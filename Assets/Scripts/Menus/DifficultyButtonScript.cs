cf49c0633eedd9fd943a0fc1ae69498b5c388509
Null Fawkes <nullfawkes@gmail.com>
2014-11-07 20:48:07 +0100

    Initial menus with difficulty added.

---
+++ Assets/Scripts/Menus/DifficultyButtonScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DifficultyButtonScript : MonoBehaviour
+{
+
+		public int difficulty;
+
+		void OnMouseDown ()
+		{
+		
+				Application.LoadLevel ("Puzzle");
+		}
+}
